c38fefcb7d82b68607104d54ca5527602ab53fec
Justin Godsey <justingodsey@justins-macbook-pro.local>
2018-11-13 15:16:41 -0600

    Created development and requestRide branches

--- A2D2_iOS/A2D2_iOS/ViewController.swift
+++ A2D2_iOS/A2D2_iOS/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var requestRideButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        requestRideButton.layer.cornerRadius = 15
+        
     }
 
 
