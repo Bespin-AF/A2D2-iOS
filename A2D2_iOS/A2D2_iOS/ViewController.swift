f95c7dc35c91550df215bf9a826bc0cd9db3c530
Justin Godsey <justingodsey@justins-macbook-pro.local>
2018-11-13 12:38:47 -0600

    Initial creation of A2D2 iOS project

---
+++ A2D2_iOS/A2D2_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A2D2_iOS
+//
+//  Created by Justin Godsey on 11/13/18.
+//  Copyright © 2018 Bespin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
