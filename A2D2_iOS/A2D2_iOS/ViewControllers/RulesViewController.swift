1db453b8fb2c1b1b6051f3d37b1be599a0678630
Justin Godsey <justingodsey@justins-macbook-pro.local>
2018-11-20 15:02:45 -0600

    Added all features for User Story number 2 "User acknowledges Rules & granted permissions" besides the navigation for the Accept button to wait until the pop up has been acknowledged to navigate.

---
+++ A2D2_iOS/A2D2_iOS/ViewControllers/RulesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RulesViewcontroller.swift
+//  A2D2_iOS
+//
+//  Created by Justin Godsey on 11/20/18.
+//  Copyright © 2018 Bespin. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Foundation
+class RulesViewController: UIViewController {
+    
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet var agreeButton: MyButton!
+    
+    
+    @IBAction func agreeButtonTapped(){
+        locationManager.requestWhenInUseAuthorization()
+        performSegue(withIdentifier: "seque_Request_Ride_Page", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- A2D2_iOS/A2D2_iOSUITests/A2D2_iOSUITests.swift
+++ A2D2_iOS/A2D2_iOSUITests/A2D2_iOSUITests.swift
@@ -7,12 +7,25 @@
 //
 
 import XCTest
+import CoreLocation
+
 class A2D2_iOSUITests: XCTestCase {
     
     var app: XCUIApplication!
+    var didAlertShow = false
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        addUIInterruptionMonitor(withDescription: "Location Dialog") { (alert) ->Bool in
+            let button = alert.buttons["Allow"]
+            self.didAlertShow = true
+            if button.exists{
+                button.tap()
+                return true
+            }
+            return false
+        }
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -28,26 +41,54 @@ class A2D2_iOSUITests: XCTestCase {
         app.terminate()
     }
     
+    let RequestRideBtn = "Request Ride"
+    let RulesAgreeBtn = "Agree"
     
     func testRequestRideView_IsShowing(){
         //Tests that Request ride view exists
         XCTAssert(app.images["a2d2logo"].exists)
-        XCTAssert(app.buttons["Request Ride"].exists)
-        XCTAssert(app.buttons["Request Ride"].isEnabled)
+        XCTAssert(app.buttons[RequestRideBtn].exists)
+        XCTAssert(app.buttons[RequestRideBtn].isEnabled)
     }
 
     
     func testRequestRideButton_DoesNavigate(){
         //Tests that the Request ride button navigates away
-        app.buttons["Request Ride"].tap()
+        app.buttons[RequestRideBtn].tap()
         XCTAssert(!app.images["a2d2logo"].exists)
-        XCTAssert(!app.buttons["Request Ride"].exists)
+        XCTAssert(!app.buttons[RequestRideBtn].exists)
     }
     
     
     func testA2D2RulesView_DoesShow(){
         //Test that the Request ride button navigates to A2D2Rules
-        app.buttons["Request Ride"].tap()
+        app.buttons[RequestRideBtn].tap()
         XCTAssert(app.staticTexts["A2D2 Program Rules"].exists)
+    }
+
+    func testAgreeButton_DoesShow(){
+        //Test that the Agree button exists
+        app.buttons[RequestRideBtn].tap()
+        XCTAssert(app.buttons[RulesAgreeBtn].exists)
+    }
+    func testAgreeButton_DoesPrompt(){
+        //Test that the Agree button prompts user with a location request pop-up
+        //CLLocationManager.authorizationStatus()
+
+        app.buttons[RequestRideBtn].tap()
+        app.buttons[RulesAgreeBtn].tap()
+        app.tap()
+        sleep(1)
+        XCTAssert(didAlertShow)
+
+    }
+    func testAgreeButton_DoesNavigate(){
+        //When Agree is selected user is navigated to the request ride page
+        app.buttons[RequestRideBtn].tap()
+        app.buttons[RulesAgreeBtn].tap()
+        app.tap()
+        sleep(1)
+        XCTAssert(app.staticTexts["Pickup Request Options"].exists)
+
     }
 }
