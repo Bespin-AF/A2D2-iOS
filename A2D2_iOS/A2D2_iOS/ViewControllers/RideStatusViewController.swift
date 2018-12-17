82bdfd82cd5be4631113a57910a051bfd7fc38ba
Jacob Miller <jacobmiller@Jacobs-MacBook-Pro.local>
2018-12-17 12:53:10 -0600

    changed button code to call A2D2

--- A2D2_iOS/A2D2_iOS/ViewControllers/RideStatusViewController.swift
+++ A2D2_iOS/A2D2_iOS/ViewControllers/RideStatusViewController.swift
@@ -8,22 +8,18 @@
 
 import Foundation
 import UIKit
-import CallKit
 
 class RideStatusViewController: UIViewController {
-    let callController = CXCallController()
     
     @IBAction func callA2D2() {
-        let uuid = UUID()
-        let handle = CXHandle(type: .phoneNumber, value: "8503561450")
-        let startCallAction = CXStartCallAction(call: uuid, handle: handle)
-        let transaction = CXTransaction(action: startCallAction)
-        callController.request(transaction) { error in
-            if let error = error {
-                print("Error requesting transaction: \(error)")
-            } else {
-                print("Requested transaction successfully")
-            }
-        }
+        let number = "8503561450"
+        let url = URL(string: "tel://\(number)")!
+        UIApplication.shared.open(url)
+        
+        print(UIApplication.shared.canOpenURL(url))
+        
+        
+        
+        
     }
 }
