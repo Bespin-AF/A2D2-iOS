f5895bab99924dfe0c312327ba1dac02a4bed45f
Daniel Crean <daniel.crean.developer@gmail.com>
2019-04-24 12:08:43 -0500

    Added Splash Page; Eliminates long loads in-app

---
+++ A2D2_iOS/A2D2_iOS/Controllers/ViewControllers/Loading_Controller.swift
@@ -0,0 +1,33 @@
+//
+//  Loading_Controller.swift
+//  A2D2_iOS
+//
+//  Created by Daniel Crean on 4/24/19.
+//  Copyright © 2019 Bespin. All rights reserved.
+//
+
+import UIKit
+
+class Loading_Controller : UIViewController, DataSourceDelegate {
+    
+    var didGetResources = false
+    var didGetRequests = false
+    
+    override func viewDidLoad() {
+        DataSourceUtils.resources.delegate = self
+        DataSourceUtils.requests.delegate = self
+    }
+
+    
+    func dataSource(_ dataSource: DataSource, dataValues: [String : Any]) {
+        if dataSource === DataSourceUtils.resources {
+            didGetResources = true
+        } else if dataSource === DataSourceUtils.requests {
+            didGetRequests = true
+        }
+        
+        if didGetRequests && didGetResources {
+            self.performSegue(withIdentifier: "segue_show_home", sender: self)
+        }
+    }
+}
